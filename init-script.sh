5860d30305814303302b01432e3d4456fd491865
toshihirock <toshihiro308@gmail.com>
2015-09-26 17:42:50 +0900

    finish test

--- init-script.sh
+++ init-script.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -ex
+
+# wait executing user_data
+sleep 30
 
 # install packages
 sudo yum -y install git docker
@@ -9,10 +12,6 @@ sudo chkconfig --add docker
 
 # install chefDK
 wget https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.8.0-1.el6.x86_64.rpm
-sudo rpm --test chefdk-0.8.0-1.el6.x86_64.rpm
 sudo rpm -Uvh chefdk-0.8.0-1.el6.x86_64.rpm
 chef verify
 echo "eval \"$(chef shell-init `basename $SHELL`)\"" >> ~/.bashrc
-
-# option
-docker pull toshihirock/amzn:2015.09
